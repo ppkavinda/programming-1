88485348b61662d54ca9734be867c8fc2ec5e732
prasad kavinda <pp.kavinda@gmail.com>
2017-04-15 11:09:49 +0530

    created folder programmes

--- fibonacci.scala
+++
@@ -1,9 +0,0 @@
-def fibonacci(n:Int):Int = {
-    n match{
-        case x if x == 0 => 0
-        case x if x == 1 => 1
-        case x => fibonacci(n - 1) + fibonacci(n - 2)
-    }
-}
-
-println(fibonacci(50))
