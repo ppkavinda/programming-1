51ab9212a8a5ff8173d41f6b1cd5c82f2512cdb4
ppkavinda <pp.kavinda@gmail.com>
2017-06-04 11:28:35 +0530

    done 8 on tute6

--- prorammes/tutes/tute_6.scala
+++ prorammes/tutes/tute_6.scala
@@ -49,8 +49,53 @@ def twos(m:Int, n:Int){
 twos(20 ,30)
 
 // 6
-def charOrder(word:String ) = {
-    for(l <- word){
-        print(l)
+def charOrder(sample:String, txt:String) = {
+    var text = txt
+    var ans:Boolean = true
+
+    for(sl <- sample){
+        var i = 0
+        if(text.length() == 0 ) println("Not Match.")
+        while(i < text.length()){
+            println(text(i) + sl.toString)
+            if (text(i) == sl) {
+                text = text.substring(i, text.length())
+                println("1")
+            }
+            i += 1
+        }
     }
 }
+charOrder("b", "abCd")
+
+// 7
+def guess() = {
+    print("Think a number with in range 0 & 100")
+    print("\ntype g for greate rthan\nl for less than\ne for equal\n")
+    var guess = 50
+    var count = 0
+    var hit = false
+    var range = 25
+    var hint = ""
+
+    while(!hit){
+        count += 1
+        println("Is it < or > or = , " + guess.toString + " ?\n")
+        hint = scala.io.StdIn.readLine
+
+        if (hint == "g" || hint == "G") {
+            guess += range
+        }else if(hint == "l" || hint == "L"){
+            guess -= range
+        }else if(hint == "e" || hint == "E"){
+            print(guess.toString + " is the number you guessed!")
+            print("\nit took " +count.toString+ " guesses to find it.")
+            hit = true
+        }
+        if(range != 1) range = range / 2
+
+
+    }
+}
+
+guess()
